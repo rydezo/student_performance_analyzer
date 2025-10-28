8c9e0bc43a29c7fc69fd5cc5259069bf11b8f944
ryder <ryderortiz7@gmail.com>
2025-10-28 11:22:26 -0400

    state and course fields

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from dataclasses import dataclass
 from drafter import *
 
 @dataclass
+class Course:
+    course_name: str
+    current_grade: float
+    test_scores: list[float]
+
+@dataclass
 class State:
     # add welcome message
     student_name: str
-    test_scores: list[float]
     target_GPA: float
+    current_GPA: float
+    courses = list[Course]
 
 @route
 def index(state: State):

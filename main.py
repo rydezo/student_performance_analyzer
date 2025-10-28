cd538dc48c70dcfd41f33c455f4e01540769e791
ryder <ryderortiz7@gmail.com>
2025-10-27 23:32:49 -0400

    initialize index route

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ from drafter import *
 
 @dataclass
 class State:
+    # add welcome message
     student_name: str
     test_scores: list[float]
     target_GPA: float
+
+@route
+def index(state: State):
+    return Page(
+        state,
+        content=[
+            # add welcome message
+            Header(f"Welcome, {state.student_name}")]
+    )
 
 
 # start_server()

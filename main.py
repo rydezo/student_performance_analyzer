e7aab092ca9677a90de14063a397f6141a0a89dd
ryder <ryderortiz7@gmail.com>
2025-11-04 12:21:53 -0500

    add_course and append_course

--- main.py
+++ main.py
@@ -12,19 +12,20 @@ class Course:
 @dataclass
 class State:
     student_name: str
-    target_GPA: float
     current_GPA: float
+    target_GPA: float
     is_failing: bool
     courses: list[Course]
     threshold_test_scores: list[int]
 
+# page 1
 @route
 def index(state: State) -> Page:
     return Page(
         state,
         content=[
             # add welcome message
-            Header(f"Welcome, {state.student_name}"),
+            Header(f"Welcome, {state.student_name}."),
             f"Your GPA: {state.current_GPA}",
             Button("Add Course", "/add_course"),
             Button("Remove Course", "/remove_course"),
@@ -33,22 +34,51 @@ def index(state: State) -> Page:
             Button("View Progress", "/view_progress")]
     )
 
+# page 2
 @route
 def add_course(state: State) -> Page:
-    pass
+    return Page(
+        state,
+        content=[
+            "Name of Course:", TextBox(name="course_name"),
+            "Number of Credits:", TextBox(name="credits", default_value=3),
+            "Current Grade:", TextBox(name="current_grade", default_value=100.0),
+            Button(text="Add Course", url="/append_course"),
+            Button(text="Cancel", url="/index")
+        ]
+    )
 
+# route 7
+@route
+def append_course(state: State, course_name: str, credits: int, current_grade: float) -> Page:
+    new_course = Course(course_name, credits, current_grade, [])
+    state.courses.append(new_course)
+    return index(state)
+
+# page 3
 @route
 def remove_course(state: State) -> Page:
     pass
 
+# page 4
 @route
 def view_courses(state: State) -> Page:
-    pass
+    if not state.courses:
+        return Page(
+            state,
+            ["You currently have no courses. Please add some to view them.",
+             Button("Add Course", "/add_course"),
+             Button("Go to Home", "/index")]
+        )
+    
+    # display courses here
 
+# page 5
 @route
 def add_test_score(state: State) -> Page:
     pass
 
+# page 6
 @route
 def view_progress(state: State) -> Page:
     if state.is_failing:
